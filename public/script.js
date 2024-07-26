0e4d54c189092e0fd2d9712c086ae5f5533f35f6
MbusyaNgesa <theengesa@gmail.com>
2024-07-26 22:16:19 +0300

    Get ElementsById

---
+++ public/script.js
@@ -0,0 +1,34 @@
+//Examine Document Object
+// console.dir(document);
+// console.log(document.links);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title = 123;
+// console.log(document.title);
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+
+// console.log(document.all);
+// console.log(document.all[8]);
+// document.all[9].textContent = 'New List';
+
+// console.log(document.forms[0]);
+// console.log(document.links)
+
+//GET ELEMENT BY ID
+// console.log(document.getElementById('header-title'))
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello World';
+// headerTitle.innerText= "Goodbye";
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h4>Welcome</h4>'
+// header.style.borderBottom = 'solid 3px #000';
+
+//GETELEMENTSBYCLASSNAME
